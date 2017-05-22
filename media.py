ea6e86aafaadda6f4e821f669216982b6c0f00dc
Nicolas <nico.hemidy@gmail.com>
2017-05-22 15:02:11 +0100

    update media.py comment format

--- media.py
+++ media.py
@@ -1,5 +1,7 @@
 class Movie():
-    # This movie class will be used to create movie instances with the following properties: a movie title, a poster url and a youtube trailer url.
+    """
+    This movie class will be used to create movie instances with the following properties: a movie title, a poster url and a youtube trailer url.
+    """
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
         self.poster_image_url = poster_image_url
